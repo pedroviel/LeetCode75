cc8b7cd3341db32c0b116be9e0816725cb7ed254
Pedro Viel <pedro.viel87@icloud.com>
2025-01-29 13:32:48 -0300

    move zeroes solution

---
+++ 283. Move Zeroes/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        int[] nums = {0,1,0,3,12};
+
+        solution.moveZeroes(nums);
+    }
+}
